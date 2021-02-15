7f854c772621c98192255a08e3ea5a1e6b47483a
Yuliya Barysevich <barysevich.yuliya@gmail.com>
2021-02-15 14:29:33 -0800

    challenge 5 solved

--- code-challenges/challenges-01.test.js
+++ code-challenges/challenges-01.test.js
@@ -101,6 +101,10 @@ const addValues = (arr, value) => {
 
 const addNumbers = (num, arr, times, callback) => {
   // Solution code here...
+  for (let i = 0; i < times; i++){
+    callback(arr, num)
+  }
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -198,7 +202,7 @@ describe('Testing challenge 4', () => {
   });
 });
 
-xdescribe('Testing challenge 5', () => {
+describe('Testing challenge 5', () => {
   test('It should add the number 8 to the array five times', () => {
     expect(addNumbers(8, [], 5, addValues)).toStrictEqual([8, 8, 8, 8, 8]);
     expect(addNumbers(8, [], 5, addValues).length).toStrictEqual(5);
