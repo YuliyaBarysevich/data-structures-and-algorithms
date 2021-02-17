efe3106edd6bba27edc64bd09a9a343d67f96674
Yuliya Barysevich <barysevich.yuliya@gmail.com>
2021-02-16 19:41:44 -0800

    challenge 5 solved

--- code-challenges/challenges-02.test.js
+++ code-challenges/challenges-02.test.js
@@ -84,6 +84,9 @@ console.log(people[1].isAuthor) prints true
 
 const setStatusAsAuthor = (people) => {
   // Solution code here...
+  people.forEach(function(object){
+    object.isAuthor = true;
+  })
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -151,7 +154,7 @@ describe('Testing challenge 4', () => {
   });
 });
 
-xdescribe('Testing challenge 5', () => {
+describe('Testing challenge 5', () => {
   test('It should add a property to every object in an array', () => {
     const a = [{ fullName: 'Octavia Butler' }, { fullName: 'Ray Bradbury' }, { fullName: 'Kurt Vonnegut' }];
     setStatusAsAuthor(a);
