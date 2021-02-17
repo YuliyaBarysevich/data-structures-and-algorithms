5860ac969d4d5f265c3404c0eaba77bbf96ce5db
Yuliya Barysevich <barysevich.yuliya@gmail.com>
2021-02-16 19:49:59 -0800

    challenge 6 solved

--- code-challenges/challenges-02.test.js
+++ code-challenges/challenges-02.test.js
@@ -107,6 +107,10 @@ console.log(a) prints [1, 2, 3, 4]
 const append = (arr1, arr2) => {
   // Solution code here...
 
+  arr2.forEach(function(num){
+    arr1.push(num);
+  })
+
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -165,7 +169,7 @@ describe('Testing challenge 5', () => {
   });
 });
 
-xdescribe('Testing challenge 6', () => {
+describe('Testing challenge 6', () => {
   test('It should append the second array to the first', () => {
     const a = [1, 2, 3, 4];
     const b = [5, 6, 7, 8];
