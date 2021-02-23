9aa576c52a0fd7cd9099676405ff329343134235
Yuliya Barysevich <barysevich.yuliya@gmail.com>
2021-02-23 13:48:37 -0800

    chellenge 4 solved

--- code-challenges/challenges-07.test.js
+++ code-challenges/challenges-07.test.js
@@ -49,6 +49,9 @@ For example, twoToThe([1,2,3]) returns [2,4,8] because 2 ^ 1 = 2, 2 ^ 2 = 4, and
 
 const forLoopTwoToThe = (arr) => {
   // Solution code here...
+  let resultArr = arr.map(number => 
+    Math.pow(2, number));
+  return resultArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -221,7 +224,7 @@ describe('Testing challenge 3', () => {
   });
 });
 
-xdescribe('Testing challenge 4', () => {
+describe('Testing challenge 4', () => {
   test('It should return two raised to the power of the integer', () => {
     expect(forLoopTwoToThe([0, 4, 5])).toStrictEqual([1, 16, 32]);
     expect(forLoopTwoToThe([0, 4, 5]).length).toStrictEqual(3);
