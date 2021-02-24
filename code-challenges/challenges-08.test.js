925fc6f7dcfc8746bc7e15c0b5bb0c492b85ab16
Yuliya Barysevich <barysevich.yuliya@gmail.com>
2021-02-24 13:38:54 -0800

    challenge 4 solved

--- code-challenges/challenges-08.test.js
+++ code-challenges/challenges-08.test.js
@@ -63,6 +63,14 @@ For example, oddValues([1,2,3]) returns [1,3].
 
 const oddValues = (arr) => {
   // Solution code here...
+  const newArr = arr.filter(num => {
+    if(num % 2 === 1){
+      return true;
+    } else {
+      return false;
+    }
+  })
+  return newArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -249,7 +257,7 @@ describe('Testing challenge 3', () => {
   });
 });
 
-xdescribe('Testing challenge 4', () => {
+describe('Testing challenge 4', () => {
   test('It should return an array containing only odd integers', () => {
     expect(oddValues([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).toStrictEqual([1, 3, 5, 7, 9]);
     expect(oddValues([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]).length).toStrictEqual(5);
