830ea25f8cfb109e640cf964213294301c847355
Yuliya Barysevich <barysevich.yuliya@gmail.com>
2021-03-03 16:46:09 -0500

    challenge 4 solved

--- code-challenges/challenges-13.test.js
+++ code-challenges/challenges-13.test.js
@@ -66,6 +66,12 @@ For example, (123) 456-7890 returns 1234567890
 
 const standardizePhoneNumbers = (arr) => {
   // Solution code here...
+  let resultArr = []
+  arr.forEach(number => {
+    let resultStr = number.replace(/[()-]|\s/g, '')
+    resultArr.push(resultStr)
+  })
+  return resultArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -209,7 +215,7 @@ describe('Testing challenge 3', () => {
   });
 });
 
-xdescribe('Testing challenge 4', () => {
+describe('Testing challenge 4', () => {
   test('It should return a standardized set of phone numbers', () => {
     const nums = ['(123) 456-7890', '(222) 222-2222'];
 
