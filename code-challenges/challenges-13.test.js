3ccccc6ee62b57b04cc5e30cd8dcbba7af6951c8
Yuliya Barysevich <barysevich.yuliya@gmail.com>
2021-03-04 01:04:09 -0500

    challenge 7 solved

--- code-challenges/challenges-13.test.js
+++ code-challenges/challenges-13.test.js
@@ -115,6 +115,13 @@ Write a function named findAnything that takes in an array of strings, along wit
 
 const findAnything = (arr, target) => {
   // Solution code here...
+  let result = [];
+  arr.forEach(string => {
+    if(string.includes(target)){
+      result.push(string);
+    }
+  });
+  return result;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -254,7 +261,7 @@ describe('Testing challenge 6', () => {
   });
 });
 
-xdescribe('Testing challenge 7', () => {
+describe('Testing challenge 7', () => {
   test('It should find all the strings that contain a given string', () => {
     const words = ['things', 'apple (:)', ':)banana', 'missing that thing', 'cant:)aloupe'];
 
